d6f10858118f74e0e0dbe06c637f09aa07c510b2
Yelro <50973739+Yelro@users.noreply.github.com>
2023-11-11 17:47:50 -0500

    No ticket: renombrar funciones (#7)

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,26 +1,21 @@
-import { sum, subtract, multiply, divide } from '../src';
+import { suma, resta, multiplicacion } from '../src';
 
 describe('blah', () => {
   it('works', () => {
-    expect(sum(1, 1)).toEqual(2);
+    expect(suma(1, 1)).toEqual(2);
   });
 });
 
 describe('blah', () => {
   it('works', () => {
-    expect(subtract(1, 1)).toEqual(2);
+    expect(resta(1, 1)).toEqual(2);
   });
 });
 
 describe('blah', () => {
   it('works', () => {
-    expect(multiply(1, 1)).toEqual(2);
+    expect(multiplicacion(1, 1)).toEqual(2);
   });
 });
 
-describe('blah', () => {
-  it('works', () => {
-    expect(divide(1, 1)).toEqual(2);
-  });
-});
 
