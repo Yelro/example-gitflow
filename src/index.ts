b542d45b9b41a9b1b64d1fc28a03ffb650ea4d05
Yelro <50973739+Yelro@users.noreply.github.com>
2023-11-11 17:48:05 -0500

    No ticket: renombrar funciones (#6)

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-export const sum = (a: number, b: number) => {
+export const suma = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('boop');
   }
   return a + b;
 };
 
-export const subtract = (a: number, b: number) => {
+export const resta = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('boop');
   }
   return a - b;
 };
 
-export const multiply = (a: number, b: number) => {
+export const multiplicacion = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('boop');
   }
