54753488cf56a5d138e4bcf330b359b6d163d225
Yelro <vanorley20@gmail.com>
2023-11-12 00:15:08 -0500

    No ticket: update

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { sum, subtract, multiply } from '../src';
-=======
-import { sum, subtract } from '../src';
->>>>>>> main
 
 describe('blah', () => {
   it('works', () => {
@@ -15,12 +11,9 @@ describe('blah', () => {
     expect(subtract(1, 1)).toEqual(2);
   });
 });
-<<<<<<< HEAD
 
 describe('blah', () => {
   it('works', () => {
     expect(multiply(1, 1)).toEqual(2);
   });
 });
-=======
->>>>>>> main
